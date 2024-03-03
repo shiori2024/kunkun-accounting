7668bdd8289a2852eb16a466088f7e18981afc24
shiori2024 <shiori2024@163.com>
2024-03-03 23:02:31 +0800

    update: AboutView add kun gif

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,6 +2,7 @@
   <div class="about">
     <h1>鲲鲲记账</h1>
     <p>一款基于 vue-cli 的 vue2 + echarts 的个人记账清单项目</p>
+    <img src="https://gd-hbimg.huaban.com/263d6ec05ebfe8dd914d49fede4d7fa0098602a1140022-WmX7V0_fw658" alt="GIF" width="300px" height="200px">
     <p>本项目采用前后端分离的架构</p>
     <p>前端项目地址：<a href="https://github.com/shiori2024/kunkun-accounting-client">{{ frontText }}</a></p>
     <p>后端项目地址：<a href="https://github.com/shiori2024/kunkun-accounting-server">{{ backText }}</a></p>
